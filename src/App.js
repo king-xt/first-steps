396bba59fcf0160a8b779d984052447b41fd56fb
christian.chilel <christianboy.ch@gmail.com>
2018-04-06 17:59:12 -0500

    Commit basic example of click events and component state for christian

--- src/App.js
+++ src/App.js
@@ -2,13 +2,34 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+
+
 class App extends Component {
+  state = { 
+    isOld: true,
+    
+  }
+
+  greet(name) {
+    console.log("Hi, " + name)
+  }
   render() {
+
+    // normal javascript 
+    var isOld = this.state.isOld
+
+    var toggleOldness = () => this.setState({isOld: !isOld})
+
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome Home</h1>
+
+          <img src={logo} 
+            className="App-logo" 
+            alt="logo" 
+            onClick={() => this.greetSomeone(prompt())} />
+
+          <h1 className="App-title">Console greeting!</h1>
         </header>
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
